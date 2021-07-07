b283e751ef210f571f6814eafc602402742fd5c9
hbarzan94 <hbarzan94@gmail.com>
2021-07-07 19:18:48 +0300

    more explicit test script

--- epd/epd_test_script.m
+++ epd/epd_test_script.m
@@ -1,17 +1,22 @@
 
 dataset_folder  = 'c:\_data\eeg\dots_30\dots_30_001';
 dataset_name    = 'dots_30_001';
+trial_structure = '128,150,129,1/2/3,131,132';
 
 % load the dataset and the trial info
 epd = epd_open([dataset_folder filesep dataset_name '.epd']);
 eti = epd_load_trial_info([dataset_folder filesep dataset_name '.eti']);
 
 % create a trial structure
-trials = epd_parse_trial_structure(epd, '128,150,129,1/2/3,131,132', eti);
+trials = epd_parse_trial_structure(epd, trial_structure, eti);
 trials_f = epd_filter_trials(trials, 'ResponseID', '3');
 
 % get markers and load data
 markers = epd_get_markers(trials, '1/2/3');
 
 % get data
-data = epd_load_data(epd, [1, 1], markers, [ 2 ]);
+seconds_before  = 1;
+seconds_after   = 2;
+channel_indices = [ 2 ];
+
+data = epd_load_data(epd, [seconds_before, seconds_after], markers, channel_indices);
